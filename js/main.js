5ff284f13ae40dfa696cef35d3d707619f5fd588
Jamie <jamesc@raicovdesigns.com.au>
2015-10-01 21:36:00 +1000

    Updates to Todo Entry Bar, Added button

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+ $('.todoItem-1').velocity({scale: "0.2"},{duration:0});
 $(document).ready(function(){
+
     initialAnimations();
     $(".button-collapse").sideNav();
 });
 
 function initialAnimations(){
- $('.darkCard').velocity({scale: "0.5"},{duration:10});
+  
     $('.navBar nav').velocity({ top: "0" },{ duration: 1000 });
-    $('.darkCard').velocity({scale: "1"},{duration:1000});   
+    $('.todoItem-1').velocity({scale: "1"},{delay:"0.2s", duration: 1000});
+    
 }
