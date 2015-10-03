99b714d176c1f401026a82870cbe67af4da4bc7f
Jamie <jamesc@raicovdesigns.com.au>
2015-10-03 13:05:05 +1000

    removed excess jquery commands from testing, floated todoEntry to align elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
- $('.todoItem-1').velocity({scale: "0.5"},{duration:0});
 $(document).ready(function(){
-
     initialAnimations();
     $(".button-collapse").sideNav();
     $('.todoEntryButton').on("click", function(){
@@ -13,10 +11,7 @@ $(document).ready(function(){
 });
 
 function initialAnimations(){
-  
     $('.navBar nav').velocity({ top: "0" },{ duration: 800 });
-    $('.todoItem-1').velocity({scale: "1"},{delay:"0.2s", duration: 800});
-    
 }
 
 function initialCardAnimation(todoItemNumber){
